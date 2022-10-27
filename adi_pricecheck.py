105c2df8b35163730fd4dc4e1ea9a390a4dbb09e
ferdlestier <88462636+ferdlestier@users.noreply.github.com>
2022-10-27 15:04:08 -0400

    Update adi_pricecheck.py

--- adi_pricecheck.py
+++ adi_pricecheck.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import requests
 import time
 
-producturl="https://www.adidas.com/us/daily-3.0-shoes/FW7050.html"
+producturl="https://www.adidas.com/us/harden-vol.-6-shoes/GW1712.html?forceSelSize=M%209%20%2F%20W%2010"
 #producturl = input ( "Please input product url : " )
 alertprice = float ( input ( "please enter the price you need to be notify : $" ) )
 print("Your link is: "+ producturl)
@@ -12,22 +12,25 @@ print ( f"Thank You, You'll be alerted if the price drop below ${alertprice}" )
 def checkPrice(producturl) :
     try:
         driver = webdriver.Chrome ()
-        driver.get ("https://www.adidas.com/us/daily-3.0-shoes/FW7050.html")
-        producttitle=driver.find_element(By.XPATH,'/html/body/div[2]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div/h1/span').text
-        productprice=driver.find_element(By.XPATH,'/html/body/div[2]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div').text
-        if float(productprice) < alertprice:
-            print(f'{productprice} price has dropped below alert price, its price now is ${productprice}')
+        driver.get ("https://www.adidas.com/us/harden-vol.-6-shoes/GW1712.html?forceSelSize=M%209%20%2F%20W%2010")
+        producttitle=driver.find_element(By.XPATH,'/html/body/div[2]/div/div[1]/div[1]/div/div/div[2]/div[2]/div[2]/div/h1/span').text
+        productprice=driver.find_element(By.XPATH,'/html/body/div[2]/div/div[1]/div[1]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/div/div').text
+        prodprice = productprice.strip('$')
+        if float(prodprice) < alertprice:
+            print(f'{producttitle} price has dropped below alert price, its price now is {productprice}')
     except:
         print('The price is still over $'+str(alertprice))
 
-#def send_message(bot_message) :
-#    bot_token = '5***************8'
-#    bot_chatID = 'BotTest'
-#    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-#    response = requests.get ( send_text )
-#    return response.json ( )
+def send_message(bot_message) :
+    bot_token = ''
+    bot_chatID = ''
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get ( send_text )
+    return response.json ( )
 
 #while(True):
 #    checkPrice(producturl)
 #    time.sleep(10)
 checkPrice(producturl)
+new_message = f'{producttitle} price has dropped below alert price, its price now is {productprice}. {producturl}'
+send_message(new_message)
